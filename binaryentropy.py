7cfd96245d71a47e6de040735c64d09bb01ded4d
Tom Johnston <tbjohnston@gmail.com>
2013-01-22 14:53:08 -0800

    Switched from math to numpy

--- binaryentropy.py
+++ binaryentropy.py
@@ -1,19 +1,19 @@
 # Binary entropy calculator
 # 21 Jan 2013
 
-import math
+import numpy as np
 
 def H2(px):
-    return (-px*math.log(px,2) + (px-1)*math.log((1-px),2))
+    return (-px*np.log(px) + (px-1)*np.log(1-px)) / np.log(2)
     
 def H_x(px):
-    return -px * math.log(px,2)
+    return -px * np.log(px)/np.log(2)
 
 print 'Calculates the entropy of three bent coins.'
 print 'The first one determines which of the next two to flip.'
 print 'Enter the values for f, g, h [All between 0 and 1] separated by commas'
 print 
-f, g, h = raw_input('Values of f g h: ').split(',')
+f, g, h = raw_input('Values of f, g, h: ').split(',')
 f, g, h = [float(x) for x in [f, g, h]]
 
 print 'Entropy of first coin H2(f): %s' % H2(f)
